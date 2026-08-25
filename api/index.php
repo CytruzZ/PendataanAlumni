2e427b8ef1cd612f0876409fe0eb1c8c460aa22f
CytruzZ <elvridtz@gmail.com>
2026-08-25 22:29:49 +0700

    Auto create /tmp storage directories for Vercel serverless

--- api/index.php
+++ api/index.php
@@ -1,4 +1,23 @@
 <?php
 
+// Fix storage paths for Vercel Serverless environment
+$tmpDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+];
+
+foreach ($tmpDirs as $dir) {
+    if (!is_dir($dir)) {
+        @mkdir($dir, 0777, true);
+    }
+}
+
+putenv('VIEW_COMPILED_PATH=/tmp/storage/framework/views');
+putenv('CACHE_DRIVER=array');
+putenv('SESSION_DRIVER=cookie');
+putenv('LOG_CHANNEL=stderr');
+
 // Forward Vercel requests to normal Laravel public/index.php
 require __DIR__ . '/../public/index.php';
